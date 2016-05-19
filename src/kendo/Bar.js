1edb27f102c25f57a5bcba56622fffffd5510db9
Shah Altaf <shah.altaf@rbi.co.uk>
2016-05-19 15:31:59 +0100

    Better implementation of interfaces

--- src/kendo/Bar.js
+++ src/kendo/Bar.js
@@ -1,94 +1,103 @@
 "use strict";
 (function (root, factory) {
     if (typeof define === "function" && define.amd) {
-        define(["jquery","kendo.all.min","../common/HTMLWidget"], factory);
+        define(["jquery","kendo.all.min","../common/CanvasWidget", "../api/INDChart"], factory);
     } else {
         root.kendo_Bar = factory();
     }
-}(this, function (jquery, kendo, HTMLWidget) {
-
+}(this, function ($, kendo, CanvasWidget, INDChart) {
 
 
     function Bar() {
-        HTMLWidget.call(this);
+        CanvasWidget.call(this);
+        INDChart.call(this);
 
-        var context = this;
         this._tag = "div";
-        this._type = "unknown";
-        this._chartType = "BarChart";
+
     }
-    Bar.prototype = Object.create(HTMLWidget.prototype);
+    Bar.prototype = Object.create(CanvasWidget.prototype);
     Bar.prototype.constructor = Bar;
     Bar.prototype._class += " kendo_Bar";
+    Bar.prototype.implements(INDChart.prototype);
 
+    Bar.prototype.publish("max", 100, "number", "Maximum",null,{tags:["Basic"]});
 
-    Bar.prototype.init = function (domNode, element) {  };
+    function createKendoChart(context, domNode){
 
-
-    Bar.prototype.update = function (domNode, element) {
-
-        var data = this.data();
         var kendoCategories = [];
         var kendoSeries = [];
-        this.data().forEach(function(ele, ind, arr){
+        context.data().forEach(function(ele, ind, arr){
             kendoCategories.push(ele[0]);
 
         });
 
-        this.columns().forEach(function(ele, ind, arr){
+        context.columns().forEach(function(ele, ind, arr){
             if(ind > 0){
 
                 var correspondingData = [];
-                data.forEach(function(dEle, dInd, dArr){
-                   correspondingData.push(dEle[ind]);
+                context.data().forEach(function(dEle, dInd, dArr){
+                    correspondingData.push(dEle[ind]);
                 });
                 kendoSeries.push({name:arr[ind], data:correspondingData});
             }
         });
 
-        var create = function() {
 
+        var kendoChartOptions = {
+            renderAs: "canvas",
+            height: "100%",
+            legend: {
+                visible: false
+            },
+            seriesDefaults: {
+                type: "bar"
+            },
+            series:kendoSeries,
 
-                var kendoChartOptions = {
-                    legend: {
-                        visible: false
-                    },
-                    seriesDefaults: {
-                        type: "bar"
-                    },
-                    series:kendoSeries,
-
-                    valueAxis: {
-                        max: 100,
-                        line: {
-                            visible: false
-                        },
-                        minorGridLines: {
-                            visible: true
-                        },
-                        labels: {
-                            rotation: "auto"
-                        }
-                    },
-                    categoryAxis: {
-                        categories: kendoCategories,
-                        majorGridLines: {
-                            visible: false
-                        }
-                    },
-                    tooltip: {
-                        visible: true,
-                        template: "#= series.name #: #= value #"
-                    }
-                };
-
-                $(domNode).kendoChart(kendoChartOptions);
-
+            valueAxis: {
+                max: context.max(),
+                line: {
+                    visible: false
+                },
+                minorGridLines: {
+                    visible: true
+                },
+                labels: {
+                    rotation: "auto"
+                }
+            },
+            categoryAxis: {
+                categories: kendoCategories,
+                majorGridLines: {
+                    visible: false
+                }
+            },
+            tooltip: {
+                visible: true,
+                template: "#= series.name #: #= value #"
+            }
         };
 
-        $(document).ready(create);
-        $(document).bind("kendo:skinChange", create);
+        $(domNode).kendoChart(kendoChartOptions);
+    }
 
+
+
+    Bar.prototype.update = function (domNode, element) {
+
+        createKendoChart(this, domNode);
+        CanvasWidget.prototype.update.apply(this, arguments);
+    };
+
+    Bar.prototype.enter = function (domNode, element) {
+        createKendoChart(this, domNode);
+        CanvasWidget.prototype.enter.apply(this, arguments);
+    };
+
+    Bar.prototype.exit = function (domNode, element){
+
+        kendo.destroy($(domNode));
+        CanvasWidget.prototype.exit.apply(this, arguments);
     };
 
     return Bar;
